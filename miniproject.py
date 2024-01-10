e8d3dce96b00082a099ee7546c4882e7393ee27b
erdemsennik <101670644+erdemsennik@users.noreply.github.com>
2024-01-10 14:15:53 -0500

    Add files via upload

--- miniproject.py
+++ miniproject.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd 
 import numpy as np 
 # import matplotlib.pyplot as plt 
-import seaborn as sns
-import math 
+# import seaborn as sns
+# import math 
 
 # # Title
 # st.title('Webapp using Streamlit')
