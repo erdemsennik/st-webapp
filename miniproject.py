1201e065a0fb3288e76d838f26375fecb8abdd1f
erdemsennik <101670644+erdemsennik@users.noreply.github.com>
2024-01-11 15:17:16 -0500

    Update miniproject.py

--- miniproject.py
+++ miniproject.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd 
 import numpy as np
-# from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder
 # from sklearn.preprocessing import LabelEncoder 
 # import matplotlib.pyplot as plt 
 # import seaborn as sns
